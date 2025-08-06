be1e538e92501978fc27375d3a5c3826f9d89802
Nguyễn Triết <trietnguyen23012003@gmail.com>
2025-08-07 01:21:43 +0700

    update

--- transfer_learning.py
+++ transfer_learning.py
@@ -1,39 +1,16 @@
 import torch
 from torchvision.models import resnet50 , ResNet50_Weights
 import torch.nn as nn
+from torchsummary import summary
 
-class MyResnet50(nn.Module):
-    def __init__(self , num_classes = 2):
-        super().__init__()
-        self.model = resnet50(weights=ResNet50_Weights.DEFAULT)
-        del self.model.fc
-        self.fc1 = nn.Linear(in_features=2048 , out_features=1024)
-        self.fc2 = nn.Linear(in_features=1024 , out_features=num_classes)
+model = resnet50(ResNet50_Weights.DEFAULT)
 
-    def _forward_impl(self, x):
-        x = self.model.conv1(x)
-        x = self.model.bn1(x)
-        x = self.model.relu(x)
-        x = self.model.maxpool(x)
+model.fc = nn.Linear(in_features=2048 , out_features=2)
+for name , params in model.named_parameters():
+    if "fc." in name or "layer4." in name:
+        pass
+    else:
+        params.requires_grad = False
+    print(name , params.requires_grad)
 
-        x = self.model.layer1(x)
-        x = self.model.layer2(x)
-        x = self.model.layer3(x)
-        x = self.model.layer4(x)
-
-        x = self.model.avgpool(x)
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = self.fc2(x)
-
-        return x
-
-    def forward(self, x):
-        return self._forward_impl(x)
-
-if __name__ == "__main__":
-
-    image = torch.rand(2 , 3 , 224 , 224)
-    model = MyResnet50()
-    output = model(image)
-    print(output.shape)
+summary(model , (3 , 224 , 224))
